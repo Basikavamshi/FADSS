cccc4f0408712e82d43e0450254c9e0cd87cf1b8
Basikavamshi <baikavamshi@gmail.com>
2025-11-26 14:50:20 +0530

    nav bar updated

--- my-app/src/app/Navbar.tsx
+++ my-app/src/app/Navbar.tsx
@@ -1,18 +1,20 @@
 
 import Link from "next/link"
-
+import { Menu } from 'lucide-react';
 function Navbar() {
   return (
-    <div className="grid border-2 grid-cols-[1fr] grid-rows-[1fr] w-1/1 h-1/1 box-border p-3.5">
+    <div className="grid border-2 grid-cols-[1fr] grid-rows-[1fr] w-1/1 h-1/1 box-border p-3.5 justify-center items-center align-middle">
         <div className="grid grid-flow-col grid-rows-[1fr] gap-2 h-1/1 w-1/1">
-           <div>
-              <span>Logo</span>
+           <div className="grid ">
+              <span>FADSS</span>
            </div>
-           <div className="sm:text-amber-300 sm:text sm2:text-red-400"><span>chatbot</span></div>
-           <div><span>wheather</span></div>
-           <div><span>livechat</span></div>
-           <div><span>menu</span></div>
-           <div><Link href={'/login'}><span>Login</span></Link></div>
+          <div className="grid grid-flow-col grid-rows-[1fr] justify-evenly justify-items-center-safe align-middle self-center justify-self-end h-1/1 w-1/1">
+            <div><span>chatbot</span></div>
+            <div><span>wheather</span></div>
+            <div><span>livechat</span></div>
+            <div><span><Menu/></span></div>
+            <div><Link href={'/login'}><span>Login</span></Link></div>
+           </div>
         </div>
     </div>
   )
--- my-app/src/app/welcome.tsx
+++ my-app/src/app/welcome.tsx
@@ -5,14 +5,14 @@ function Welcome() {
   const controlVideo=(time:number)=>{
     if(videoRef.current){
       videoRef.current.currentTime=time;
-      videoRef.current.pause();
+      videoRef.current.play();
     }
     
   }
   return (
     <div className='grid grid-cols-[0.6fr_0.4fr] grid-rows-[1fr] items-center justify-center justify-items-center box-border p-2.5'>
         <div className='grid grid-flow-row grid-col-[1fr]'>
-           <div><span>Welcome To FADSS -Farming Powered by </span><span>Intelligence</span></div>
+           <div><span>Welcome To FADSS -Farming Powered by </span><span className="text-red-500">Intelligence</span></div>
            <p>Experience the power of AI with smart recommendations to improve yield, reduce costs, and boost farmer income.</p>
            <ul>
               <li onClick={()=>controlVideo(200)}>crop selection</li>
